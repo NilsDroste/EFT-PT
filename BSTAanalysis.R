e955c8449a1b4768806c32c8d35c0da1285f8f6b
Nils Droste <nils.droste@ufz.de>
2017-10-11 17:18:26 -0500

    changes due to major revisions

--- BSTAanalysis.R
+++ BSTAanalysis.R
@@ -54,28 +54,24 @@ library(CausalImpact)
 
 # run model
 set.seed(2468)
-
-impact <- CausalImpact(data, pre.period, post.period)
-
+impact.base <- CausalImpact(data, pre.period, post.period, model.args = list(niter = 10000))
 
 # plot model
-
-plot(impact)
-
+plot(impact.base)
 
 # plot variable importance
+plot(impact.base$model$bsts.model, "coefficients", inclusion.threshold = 0)
 
-plot(impact$model$bsts.model, "coefficients", inclusion.threshold = 0)
-
-  # probability of positive sign
-coefplot <- plot(impact$model$bsts.model, "coefficients", inclusion.threshold = 0)
-round(plot1$positive.prob, 2)
-
+# probability of positive sign
+coefplot <- plot(impact.base$model$bsts.model, "coefficients", inclusion.threshold = 0)
+round(coefplot$positive.prob, 2)
 
 # obtain a summary
+summary(impact.base)
+summary(impact.base, "report")
 
-summary(impact)
-summary(impact, "report")
+# display confidence intervals of estimates for post-intervention
+round(impact$series[c(14,20),c(4,5,10,11,13,14)],3)
 
 # 3 Robustness Check ###########################################
 
@@ -83,13 +79,13 @@ summary(impact, "report")
 data2<-zoo(cbind(PAratio.no=df$PAratio.no2,VAagr=df$VAagr,VAind=df$VAind,VAser=df$VAser,GDPcap=df$GDPcap,POPdens=df$POPdens, ENGOmem=df$ENGOmem,ENVexp=df$ENVexp, ENVinc=df$ENVinc),df$year)
 
 set.seed(2468)
-impact2 <- CausalImpact(data2, pre.period, post.period)
+impact.inst <- CausalImpact(data2, pre.period, post.period, model.args = list(niter = 10000))
 
-plot(impact2)
+plot(impact.inst)
 
-summary(impact2)
+summary(impact.inst)
 
-plot(impact2$model$bsts.model, "coefficients", inclusion.threshold = 0)
+plot(impact.inst$model$bsts.model, "coefficients", inclusion.threshold = 0)
 
 
 # 3.2 Analysis on area instead of number of PAs
@@ -99,21 +95,129 @@ plot(impact2$model$bsts.model, "coefficients", inclusion.threshold = 0)
 data3<-zoo(cbind(PAratio=df$PAratio,VAagr=df$VAagr,VAind=df$VAind,VAser=df$VAser,GDPcap=df$GDPcap,POPdens=df$POPdens, ENGOmem=df$ENGOmem,ENVexp=df$ENVexp, ENVinc=df$ENVinc),df$year)
 
 set.seed(2468)
-impact3 <- CausalImpact(data3, pre.period, post.period)
+impact.area <- CausalImpact(data3, pre.period, post.period, model.args = list(niter = 10000))
 
-plot(impact3)
+plot(impact.area)
 
-summary(impact3)
+summary(impact.area)
 
-plot(impact2$model$bsts.model, "coefficients", inclusion.threshold = 0)
-
-# Area data, excluding municipal PAs only possible due to Nature Conservation Law / Competency reform 2008
+# 3.2.2. Area data, excluding municipal PAs only possible due to Nature Conservation Law / Competency reform 2008
 
 data4<-zoo(cbind(PAratio2=df$PAratio2,VAagr=df$VAagr,VAind=df$VAind,VAser=df$VAser,GDPcap=df$GDPcap,POPdens=df$POPdens, ENGOmem=df$ENGOmem,ENVexp=df$ENVexp, ENVinc=df$ENVinc),df$year) 
 
 set.seed(2468)
-impact4 <- CausalImpact(data4, pre.period, post.period)
+impact.area.inst <- CausalImpact(data4, pre.period, post.period, model.args = list(niter = 10000))
 
-summary(impact4)
+summary(impact.area.inst)
 
-plot(impact4)
+plot(impact.area.inst)
+
+
+# 3.3 a local trends model specification
+require(bsts)
+post.period.response <- df$PAratio.no[14 : 20]
+PAratio.no <- df$PAratio.no
+PAratio.no[14 : 20]<- NA
+
+## 3.3.1 local trends
+set.seed(2468)
+ss <- AddLocalLinearTrend(list(), PAratio.no)
+bsts.model <- bsts(PAratio.no ~ data$VAagr+data$VAind+data$VAser+data$GDPcap+data$POPdens+data$ENGOmem+data$ENVexp+data$ENVinc, ss, niter = 10000)
+impact.localTrend <- CausalImpact(bsts.model = bsts.model, post.period.response = post.period.response)
+summary(impact.localTrend)
+summary(impact.localTrend, "report")
+
+## 3.4.1 semi local trends  
+set.seed(2468)
+ss2 <- AddSemilocalLinearTrend(list(), PAratio.no)
+bsts.model <- bsts(PAratio.no ~ data$VAagr+data$VAind+data$VAser+data$GDPcap+data$POPdens+data$ENGOmem+data$ENVexp+data$ENVinc, ss2, niter = 10000)
+impact.semiLocalTrend <- CausalImpact(bsts.model = bsts.model, post.period.response = post.period.response)
+summary(impact.semiLocalTrend)
+summary(impact.semiLocalTrend, "report")
+
+
+# 3 Reporting Monte Carlo Standard Errors ###########################################
+
+library(plyr)
+require(LaplacesDemon)
+
+MonCarlSE <- data.frame(matrix(NA, nrow = 9, ncol = 19))
+MonCarlSE <- rename(MonCarlSE, c("X1"="Variables", "X2"="MCSE_mod1", "X3"="SD_mod1", "X4"="MCSE/SD*100_mod1", "X5"="MCSE_mod2", "X6"="SD_mod2", "X7"="MCSE/SD*100_mod2", "X8"="MCSE_mod3", "X9"="SD_mod3", "X10"="MCSE/SD*100_mod3", "X11"="MCSE_mod4", "X12"="SD_mod4", "X13"="MCSE/SD*100_mod4", "X14"="MCSE_mod5", "X15"="SD_mod5", "X16"="MCSE/SD*100_mod5", "X17"="MCSE_mod6", "X18"="SD_mod6", "X19"="MCSE/SD*100_mod6"))
+
+MonCarlSE$Variables <- c("intercept", "valued added by agriculture (VAagr)","valued added by industry (VAind)","valued added by service (VAser)", "GDP per capita (GDPcap)","population density (POPdens)","municipal environmental spending (ENVexp)","municipal environmental income (ENVinc))", "environmental NGO members per 1,000 inhabitants (ENGOmem)")
+
+for(i in c(2:19)){
+  if(i==2){
+    for(j in c(1:9)){
+    MonCarlSE[j,i] <- MCSE(impact.base$model$bsts.model$coefficients[,j])
+    }}
+  if(i==3){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- sd(impact.base$model$bsts.model$coefficients[,j])
+    }}
+  if(i==4){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.base$model$bsts.model$coefficients[,j])/sd(impact$model$bsts.model$coefficients[,j])*100
+    }}
+  if(i==5){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.inst$model$bsts.model$coefficients[,j])
+    }}
+  if(i==6){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- sd(impact.inst$model$bsts.model$coefficients[,j])
+    }}
+  if(i==7){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.inst$model$bsts.model$coefficients[,j])/sd(impact.inst$model$bsts.model$coefficients[,j])*100
+    }}
+  if(i==8){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.area$model$bsts.model$coefficients[,j])
+    }}
+  if(i==9){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- sd(impact.area$model$bsts.model$coefficients[,j])
+    }}
+  if(i==10){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.area$model$bsts.model$coefficients[,j])/sd(impact.area$model$bsts.model$coefficients[,j])*100
+    }}
+  if(i==11){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.area.inst$model$bsts.model$coefficients[,j])
+    }}
+  if(i==12){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- sd(impact.area.inst$model$bsts.model$coefficients[,j])
+    }}
+  if(i==13){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.area.inst$model$bsts.model$coefficients[,j])/sd(impact.area.inst$model$bsts.model$coefficients[,j])*100
+    }}
+  if(i==14){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.localTrend$model$bsts.model$coefficients[,j])
+    }}
+  if(i==15){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- sd(impact.localTrend$model$bsts.model$coefficients[,j])
+    }}
+  if(i==16){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.localTrend$model$bsts.model$coefficients[,j])/sd(impact.localTrend$model$bsts.model$coefficients[,j])*100
+    }}
+  if(i==17){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.semiLocalTrend$model$bsts.model$coefficients[,j])
+    }}
+  if(i==18){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- sd(impact.semiLocalTrend$model$bsts.model$coefficients[,j])
+    }}
+  if(i==19){
+    for(j in c(1:9)){
+      MonCarlSE[j,i] <- MCSE(impact.semiLocalTrend$model$bsts.model$coefficients[,j])/sd(impact.semiLocalTrend$model$bsts.model$coefficients[,j])*100
+    }}
+}
+
